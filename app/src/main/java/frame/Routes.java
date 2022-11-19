e961ec302fe1c788428e65bea7d6adf38ac7746e
Gavin Abeele <gabeele2160@conestogac.on.ca>
2022-11-19 18:55:34 +0000

    Added basic url routes template

--- app/src/main/java/frame/Routes.java
+++ app/src/main/java/frame/Routes.java
@@ -8,7 +8,43 @@ public class Routes {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "This is the index page";
 	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "This is the login page";
+	}
+
+    @GetMapping("/login-m")
+	public String manualLogin() {
+		return "This is the manual login page";
+        
+	}
+
+    @GetMapping("/menu")
+	public String menu() {
+		return "This is the menu page";
+        
+	}
+
+    @GetMapping("/balance")
+	public String balance() {
+		return "This is the balance page";
+        
+	}
+
+    @GetMapping("/withdraw")
+	public String withdraw() {
+		return "This is the withdraw page";
+        
+	}
+
+    @GetMapping("/deposit")
+	public String deposit() {
+		return "This is the deposit page";
+        
+	}
+    
 
 }
